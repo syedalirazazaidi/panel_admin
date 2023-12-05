56417dc8fae6a98a9586974bd35147d432d2b9e8
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-12-05 22:19:58 +0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <Button variant={"destructive"} className="bg-yellow-100">
+        Button
+      </Button>
+    </>
+  );
+}
