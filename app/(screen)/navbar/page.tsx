19886ce400e59f62dee5f070c9d8c255e2dd46a1
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2023-12-08 00:39:14 +0500

    configure eslint prettier

--- app/(screen)/navbar/page.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Header() {
-  return <nav>this is header</nav>;
-}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,5 @@
 import { Button } from "@/components/ui/button";
-import Login from "./(auth)/login/page";
-import SideBar from "./(screen)/sidebar/page";
-import Admin from "./(screen)/admindashboard/page";
 
 export default function Home() {
-  return (
-    <>
-      <Admin />
-    </>
-  );
+  return <>hi developer</>;
 }
